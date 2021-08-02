60694ab9e072a1c622b32c56a943c61cfb98940b
808bgit config --global user.name yourUsername <yourEmail@domain.com>
2021-08-02 09:50:51 -0700

    Initial commit

---
+++ src/test/java/com/example/springboot__security/SpringBootSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot__security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
